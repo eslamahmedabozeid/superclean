95e6d2c6572cd69e9c864cbc997c6517e5e6f1b2
eslamahmedabozeid <60574969+eslamahmedabozeid@users.noreply.github.com>
2026-09-14 10:53:08 +0300

    upload

---
+++ src/app/home/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import HeroSection from "@/components/sections/HeroSection";
+import StatsSection from "@/components/sections/StatsSection";
+import OurServicesSection from "@/components/sections/OurServicesSection";
+import ProcessSection from "@/components/sections/ProcessSection";
+import WhySuperCleanSection from "@/components/sections/WhySuperCleanSection";
+
+export default function HomePage() {
+  return (
+    <div className="w-full bg-white">
+      <HeroSection />
+      <StatsSection />
+      <OurServicesSection />
+      <ProcessSection />
+      <WhySuperCleanSection />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-import React from "react";
-import HeroSection from "@/components/home/HeroSection";
-import StatsSection from "@/components/home/StatsSection";
-import OurServicesSection from "@/components/home/OurServicesSection";
-import ProcessSection from "@/components/home/ProcessSection";
-import WhySuperCleanSection from "@/components/home/WhySuperCleanSection";
+import HomePage from "./home/page";
 
-export default function HomePage() {
-  return (
-    <div className="w-full bg-white">
-      <HeroSection />
-      <StatsSection />
-      <OurServicesSection />
-      <ProcessSection />
-      <WhySuperCleanSection />
-    </div>
-  );
+export default function Page() {
+  return <HomePage />;
 }
